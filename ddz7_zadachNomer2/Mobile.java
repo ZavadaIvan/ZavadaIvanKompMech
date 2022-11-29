3cc43ce4af3d67ee7775de69949cb3415479aa7f
ZavadaIvan <114930938+ZavadaIvan@users.noreply.github.com>
2022-11-29 04:34:50 +0300

    Delete ddz7_zadachNomer2 directory

--- ddz7_zadachNomer2/Mobile.java
+++
@@ -1,8 +0,0 @@
-package ddz7_zadachNomer2;
-
-public interface Mobile {
-    void startCall (String number);
-    String searchForCall ();
-    void acceptCall (String number);
-    void breakCall ();
-}
--- ddz7_zadachNomer2/SiemensMobile.java
+++
@@ -1,9 +0,0 @@
-package ddz7_zadachNomer2;
-
-abstract class SiemensMobile implements Mobile {
-    public Boolean incomingCall;
-    public Boolean isTalkingNow;
-    public int siemensSerialNumber;
-    abstract void connectToSiemensOnline();
-    
-}
